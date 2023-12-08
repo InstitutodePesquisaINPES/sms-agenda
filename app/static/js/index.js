983da722a6d439f4acdeac508d97c776962e4ce4
Dovani <dovanilenares@gmail.com>
2023-12-08 12:10:14 -0300

    fix(user): front end changes

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -13,3 +13,28 @@ var swiper = new Swiper(".slide-content", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var navbar = document.getElementById('navbar');
+    var lastScrollTop = 0;
+  
+    window.addEventListener('scroll', function() {
+      var currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+  
+      // Verifica se está no topo da página
+      if (currentScrollTop === 0) {
+        navbar.classList.remove('navbarToogle');
+      } else if (currentScrollTop > lastScrollTop) {
+        // Scroll para baixo
+        navbar.classList.add('navbarToogle');
+      } else if (currentScrollTop < lastScrollTop) {
+        // Scroll para cima
+        navbar.classList.remove('navbarToogle');
+      }
+  
+      lastScrollTop = currentScrollTop;
+    });
+  });
+  
+  
+  
